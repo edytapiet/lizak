60e586ab5f19062774acf057a793c40cc2e8ee52
Edyta Pietrzak <edpiet19@gmail.com>
2025-11-16 19:03:23 +0100

    Dodanie zadań do python_basic

--- zadb.py
+++ zadb.py
@@ -4,7 +4,12 @@ def pomnoz_przez_dwa(lista):
         nowa_lista.append(liczba * 2)
     return nowa_lista
 
-liczby = [1, 2, 3, 4, 5]
-wynik = pomnoz_przez_dwa(liczby)
-print(wynik)
 
+def pomnoz_przez_dwa_skladana(lista):
+    return [x * 2 for x in lista]
+
+
+liczby = [1, 2, 3, 4, 5]
+
+print(pomnoz_przez_dwa(liczby))
+print(pomnoz_przez_dwa_skladana(liczby))
---
+++ zadc.py
@@ -0,0 +1,7 @@
+def wyswietl_parzyste(lista):
+    for liczba in lista:
+        if liczba % 2 == 0:
+            print(liczba)
+
+liczby = list(range(10))
+wyswietl_parzyste(liczby)
