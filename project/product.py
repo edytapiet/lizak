a7441952986f03e2b47770f4bdf6d582abd4778d
Edyta Pietrzak <edpiet19@gmail.com>
2025-12-13 14:12:10 +0100

    init project

---
+++ project/product.py
@@ -0,0 +1,10 @@
+
+import magazine.utils
+
+
+class Product:
+    def __init__(self, name):
+        self.name = name
+
+    def show(self):
+        return f"Produkt: {self.name}, {magazine.utils.helper()}"
---
+++ project/utils.py
@@ -0,0 +1,2 @@
+def helper():
+    return "Pomoc z utils"
